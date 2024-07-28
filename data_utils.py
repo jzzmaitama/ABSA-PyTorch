61a1e76d674d29b349504cc31d4780652e62887a
jzzmaitama <jzzmaitama@gmail.com>
2024-07-28 20:50:53 +0000

    ber-spc

--- data_utils.py
+++ data_utils.py
@@ -51,8 +51,9 @@ def build_embedding_matrix(word2idx, embed_dim, dat_fname):
     else:
         print('loading word vectors...')
         embedding_matrix = np.zeros((len(word2idx) + 2, embed_dim))  # idx 0 and len(word2idx)+1 are all-zeros
-        fname = '/home/jzzmaitama/PycharmProjects/ABSA-PyTorch/glove/glove.twitter.27B/glove.twitter.27B.' + str(embed_dim) + 'd.txt' \
-            if embed_dim != 300 else './glove.42B.300d.txt'
+        # fname = '/home/jzzmaitama/PycharmProjects/ABSA-PyTorch/glove/glove.twitter.27B/glove.twitter.27B.' + str(embed_dim) + 'd.txt' \
+        #     if embed_dim != 300 else './glove.42B.300d.txt'
+        fname = "/home/jzzmaitama/PycharmProjects/ABSA-PyTorch/glove/glove.twitter.27B.200d.txt"
         word_vec = _load_word_vec(fname, word2idx=word2idx, embed_dim=embed_dim)
         print('building embedding_matrix:', dat_fname)
         for word, i in word2idx.items():
